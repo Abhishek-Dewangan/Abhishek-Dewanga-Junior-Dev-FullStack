a45a15c107765bbaf4b7e10480e0b9ae48e3281c
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-01-12 15:46:54 +0530

    server connected with mongodb databse

--- api/database/db.js
+++ api/database/db.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const mongodburl = process.env.MONGODB_URL;
 
