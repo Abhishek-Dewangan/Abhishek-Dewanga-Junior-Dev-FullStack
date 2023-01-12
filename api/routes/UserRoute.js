63d9b298b1040e08e0ac99e18c82522b902f5dba
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-01-12 15:35:38 +0530

    Logout feature added

--- api/routes/UserRoute.js
+++ api/routes/UserRoute.js
@@ -6,12 +6,12 @@ const userRouter = Router();
 
 userRouter.post('/signup', async (req, res) => {
   try {
+    const isUnique = await User.findOne({email: req.body.email});
+    isUnique && res.status(409).send({message: 'Email is already registered'});
     const user = await new User(req.body).save();
-    res
-      .status(200)
-      .send({message: `${user.name} registered successfully`, user});
+    res.status(200).send({message: `${user.name} registered successfully`});
   } catch (error) {
-    res.status(400).send(error);
+    res.status(401).send({message: 'Error while signup', error});
   }
 });
 
@@ -19,19 +19,19 @@ userRouter.post('/signin', async (req, res) => {
   try {
     const {email, password} = req.body;
     const user = await User.findOne({email});
-    console.log(user);
     if (user) {
       const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
         const token = await user.generateAuthToken();
-        console.log(user);
-        res.status(201).send({message: 'User login successfully', token, user});
+        res
+          .status(201)
+          .send({message: 'User login successfully', token, userid: user._id});
       } else {
-        res.status(400).send({message: 'Wrong Password Entered'});
+        res.status(401).send({message: 'Wrong Password Entered'});
       }
-    } else res.status(404).send({message: 'Email is not registere'});
+    } else res.status(404).send({message: 'Email is not registered'});
   } catch (error) {
-    res.status(400).send(error);
+    res.status(401).send({message: 'Error while signin', error});
   }
 });
 
@@ -39,17 +39,15 @@ userRouter.post('/logout', async (req, res) => {
   try {
     const {token} = req.headers;
     const user = await User.findOne({token: token});
-    console.log(user);
     if (user) {
       user.token = '';
       await user.save();
-      console.log(user);
       res.status(200).send({message: 'Logout successfully'});
     } else {
-      res.status(400).send({message: 'Invalid token'});
+      res.status(498).send({message: 'Invalid token'});
     }
   } catch (error) {
-    res.status(400).send(error);
+    res.status(401).send({message: 'Error while logout', error});
   }
 });
 
