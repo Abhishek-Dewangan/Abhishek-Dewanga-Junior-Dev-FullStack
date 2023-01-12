7bc3c4cb30d0e0160b13d3b4a7b1a342f386e12c
callmeabhi1997 <callmeabhi1997@gmail.com>
2023-01-12 15:40:06 +0530

    Signin form added

--- client/pages/signin.js
+++ client/pages/signin.js
@@ -13,57 +13,62 @@ const signin = () => {
     console.log(data);
   };
   return (
-    <div className='outline outline-gray-500 outline-1 rounded grid gap-5 w-fit place-content-center mx-auto mt-20 p-10'>
-      <form onSubmit={handleSubmit} className='grid gap-5'>
-        <div>
-          <label>Username</label>
-          <br />
-          <input
-            required
-            type={'text'}
-            name='username'
-            placeholder='exapmple-john123'
-            className='outline outline-1 rounded px-5 py-2 w-80'
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <br />
-          <input
-            required
-            type={passwordType}
-            name='password'
-            placeholder='*********'
-            className='outline outline-1 rounded px-5 py-2 w-80'
-          />
-          <button
-            type='button'
-            onClick={() =>
-              setPasswordType(passwordType === 'password' ? 'text' : 'password')
-            }
-          >
-            {passwordType === 'password' ? (
-              <AiFillEyeInvisible className='inline relative right-5' />
-            ) : (
-              <AiFillEye className='inline relative right-5' />
-            )}
-          </button>
-        </div>
-        <div className='text-center'>
-          <input
-            type={'submit'}
-            value='Register'
-            className='bg-black text-white px-5 py-2 w-80 mr-4 rounded'
-          />
-        </div>
-      </form>
-      <p className='text-center text-gray-600'>
-        Don't have an account?{' '}
-        <Link href={'/signup'} className='text-blue-800 font-bold'>
-          Register
-        </Link>
-      </p>
-    </div>
+    <>
+      <title>Signin</title>
+      <div className='outline outline-gray-500 outline-1 rounded grid gap-5 w-fit place-content-center mx-auto mt-20 p-10'>
+        <form onSubmit={handleSubmit} className='grid gap-5'>
+          <div>
+            <label>Email</label>
+            <br />
+            <input
+              required
+              type={'text'}
+              name='email'
+              placeholder='Eg. john@gamil.com'
+              className='outline outline-1 rounded px-5 py-2 w-80'
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <br />
+            <input
+              required
+              type={passwordType}
+              name='password'
+              placeholder='*********'
+              className='outline outline-1 rounded px-5 py-2 w-80'
+            />
+            <button
+              type='button'
+              onClick={() =>
+                setPasswordType(
+                  passwordType === 'password' ? 'text' : 'password'
+                )
+              }
+            >
+              {passwordType === 'password' ? (
+                <AiFillEyeInvisible className='inline relative right-5' />
+              ) : (
+                <AiFillEye className='inline relative right-5' />
+              )}
+            </button>
+          </div>
+          <div className='text-center'>
+            <input
+              type={'submit'}
+              value='Register'
+              className='bg-black text-white px-5 py-2 w-80 mr-4 rounded'
+            />
+          </div>
+        </form>
+        <p className='text-center text-gray-600'>
+          Don't have an account?{' '}
+          <Link href={'/signup'} className='text-blue-800 font-bold'>
+            Register
+          </Link>
+        </p>
+      </div>
+    </>
   );
 };
 
